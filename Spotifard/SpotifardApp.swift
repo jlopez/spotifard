8c2b318b018b5207b99e05bbf1e49d9cd860cfcb
Jesus Lopez <jesus@jesusla.com>
2023-11-19 19:54:21 -0800

    Initial Commit

---
+++ Spotifard/SpotifardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifardApp.swift
+//  Spotifard
+//
+//  Created by Jesus Lopez on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotifardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
